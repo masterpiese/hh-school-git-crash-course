fbff8b0b1278587b2fb70ab8740512b8c2988ba1
Nikita Bezdolniy <gooverdian@gmail.com>
2021-12-07 01:12:34 +0300

    Add bubbleSort

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,19 @@
 
 def bubbleSort(arr):
     n = len(arr)
+
     for i in range(n):
         for j in range(0, n-i-1):
+
+    # Traverse through all array elements
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
