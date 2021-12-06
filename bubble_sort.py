60e44d7a81f79b90d5c36e0049ce5b634c862296
Nikita Bezdolniy <gooverdian@gmail.com>
2021-12-07 01:16:17 +0300

    Fix! Strip bubbleSort comments

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,19 +2,23 @@
 
 def bubbleSort(arr):
     n = len(arr)
+<<<<<<< HEAD
 
     for i in range(n):
         for j in range(0, n-i-1):
 
     # Traverse through all array elements
+=======
+>>>>>>> 2db0858 (Fix! Strip bubbleSort comments)
     for i in range(n):
-
-        # Last i elements are already in place
         for j in range(0, n-i-1):
+<<<<<<< HEAD
 
             # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
 
+=======
+>>>>>>> 2db0858 (Fix! Strip bubbleSort comments)
             if arr[j] > arr[j+1] :
                 arr[j], arr[j+1] = arr[j+1], arr[j]
